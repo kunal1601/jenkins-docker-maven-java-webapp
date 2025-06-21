6a74d76f4833427960e461f3ceddd13d3d62a05a
Kunal <kunalsharma1601@gmail.com>
2025-06-21 14:44:11 +0530

    update

--- src/main/java/com/rst/helloworld/web/WelcomeController.java
+++ src/main/java/com/rst/helloworld/web/WelcomeController.java
@@ -10,43 +10,49 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class WelcomeController {
 
-	private final Logger logger = LoggerFactory.getLogger(WelcomeController.class);
-	private final HelloWorldService helloWorldService;
+    private final Logger logger = LoggerFactory.getLogger(WelcomeController.class);
+    private final HelloWorldService helloWorldService;
 
-	@Autowired
-	public WelcomeController(HelloWorldService helloWorldService) {
-		this.helloWorldService = helloWorldService;
-	}
+    @Autowired
+    public WelcomeController(HelloWorldService helloWorldService) {
+        this.helloWorldService = helloWorldService;
+    }
 
-	@RequestMapping(value = "/", method = RequestMethod.GET)
-	public String index(Map<String, Object> model) {
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String index(Map<String, Object> model) {
 
-		logger.debug("index() is executed!");
+        logger.debug("index() is executed!");
 
-		model.put("title", helloWorldService.getTitle(""));
-		model.put("msg", helloWorldService.getDesc());
-		
-		return "index";
-	}
+        model.put("title", helloWorldService.getTitle(""));
+        model.put("msg", helloWorldService.getDesc());
 
-	@RequestMapping(value = "/hello/{name:.+}", method = RequestMethod.GET)
-	public ModelAndView hello(@PathVariable("name") String name) {
+        return "index"; // JSP file name
+    }
 
-		logger.debug("hello() is executed - $name {}", name);
+    @RequestMapping(value = "/hello/{name:.+}", method = RequestMethod.GET)
+    public ModelAndView hello(@PathVariable("name") String name) {
 
-		ModelAndView model = new ModelAndView();
-		model.setViewName("index");
-		
-		model.addObject("title", helloWorldService.getTitle(name));
-		model.addObject("msg", helloWorldService.getDesc());
-		
-		return model;
+        logger.debug("hello() is executed - $name {}", name);
 
-	}
+        ModelAndView model = new ModelAndView();
+        model.setViewName("index");
 
+        model.addObject("title", helloWorldService.getTitle(name));
+        model.addObject("msg", helloWorldService.getDesc());
+
+        return model;
+    }
+
+    // ✅ Test endpoint to confirm Spring MVC is working
+    @RequestMapping(value = "/test", method = RequestMethod.GET)
+    @ResponseBody
+    public String test() {
+        return "✅ Spring is working!";
+    }
 }
